04bca52a46c90ea1071bd3a1e2ce820efef0ab26
madhan <madhan.uniqe@gmail.com>
2025-08-01 08:39:58 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "attendance-ai-xur9q",
-  appId: "1:30376040039:web:c096dda53175e0243366c2",
-  storageBucket: "attendance-ai-xur9q.firebasestorage.app",
-  apiKey: "AIzaSyAwEolx81dICW505joMYE03E6T92Bex5vg",
-  authDomain: "attendance-ai-xur9q.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "30376040039"
+  "projectId": "attendance-ai-xur9q",
+  "appId": "1:30376040039:web:c096dda53175e0243366c2",
+  "storageBucket": "attendance-ai-xur9q.firebasestorage.app",
+  "apiKey": "AIzaSyAwEolx81dICW505joMYE03E6T92Bex5vg",
+  "authDomain": "attendance-ai-xur9q.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "30376040039"
 };
 
 // Initialize Firebase
